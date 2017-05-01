6726d8a1e082b599c4531d53867901adcf363c3c
MichaelHilton <michael.hilton@gmail.com>
2017-05-01 16:10:56 -0700

    first commit

---
+++ src/main/java/Canary.java
@@ -0,0 +1,8 @@
+/**
+ * Created by michaelhilton on 5/1/17.
+ */
+public class Canary {
+    public static void main(String args[]){
+        System.out.println("Canary");
+    }
+}
---
+++ src/test/java/CanaryTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by michaelhilton on 5/1/17.
+ */
+class CanaryTest {
+
+    @Test
+    void myFirstTest() {
+        assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    void mySecondTest() {
+        assertEquals(2, 3-1);
+    }
+
+}
